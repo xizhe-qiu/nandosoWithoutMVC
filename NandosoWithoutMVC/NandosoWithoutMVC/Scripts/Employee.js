232cc379408ca231370c326ada7a4c2c8e358475
xizhe qiu <imxizhe@hotmail.com>
2015-11-26 16:41:25 +1300

    finished employee

---
+++ NandosoWithoutMVC/NandosoWithoutMVC/Scripts/Employee.js
@@ -0,0 +1,46 @@
+﻿document.addEventListener("DOMContentLoaded", function () {
+    //when the document opens this runs
+    loadEmployees();
+});
+
+function loadEmployees() {
+    EmployeeModule.getEmployees(setupEmployeesTable);
+}
+
+function setupEmployeesTable(EmployeeList) {
+    var employeeTable = document.getElementById("employeeList");
+    console.log(EmployeeList);
+
+    for (i = 0; i < EmployeeList.length; i++) {
+        //create a row
+        var row = document.createElement("tr");
+        //create columns
+        var EmployeeFirstNameCol = document.createElement("td");
+        //fill in data
+        EmployeeFirstNameCol.innerHTML = EmployeeList[i].EmployeeFirstName;
+        //append the column into the row
+        row.appendChild(EmployeeFirstNameCol);
+        var EmployeeLastNameCol = document.createElement("td");
+        EmployeeLastNameCol.innerHTML = EmployeeList[i].EmployeeLastName;
+        row.appendChild(EmployeeLastNameCol);
+        var RoleCol = document.createElement("td");
+        switch (EmployeeList[i].Role) {
+            case 0:
+                RoleCol.innerHTML = "CEO";
+                break;
+            case 1:
+                RoleCol.innerHTML = "Manager";
+                break;
+            case 2:
+                RoleCol.innerHTML = "Chef";
+                break;
+            case 3:
+                RoleCol.innerHTML = "Waiter";
+                break;
+        }
+        row.appendChild(RoleCol);
+
+        //finally append the row to the table
+        employeeTable.appendChild(row);
+    }
+}
---
+++ NandosoWithoutMVC/NandosoWithoutMVC/Scripts/EmployeeModule.js
@@ -0,0 +1,32 @@
+﻿var EmployeeModule = (function () {
+
+    //callback pushes information out 
+    return {
+        getEmployees: function (callback) {
+            // code to make api calls goes here
+
+            $.ajax({
+                type: "GET",
+                dataType: "json",
+                url: "http://nandosowithoutmvc.azurewebsites.net/api/Employees",
+                success: function (data) {
+                    callback(data);//so this is equivilent to setupStudentsTable(data)
+                }
+            });
+        },
+
+        addEmployee: function (employee, callback) {
+            var xhttp = new XMLHttpRequest();
+
+            xhttp.onreadystatechange = function () {
+                if (xhttp.readyState == 4 && xhttp.status == 201) {
+                    callback();
+                }
+            }
+
+            xhttp.open("POST", "http://nandosowithoutmvc.azurewebsites.net/api/Employees", true)
+            xhttp.setRequestHeader("Content-type", "application/json");
+            xhttp.send(JSON.stringify(employee));
+        }
+    }
+}());
